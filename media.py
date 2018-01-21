1dec3c095112bb92d8f98420380e5ac15682d552
Siddharth Janghu <siddharthj.dce@gmail.com>
2018-01-21 17:24:07 +0530

    Create a class called Movie

---
+++ media.py
@@ -0,0 +1,8 @@
+import webbrowser
+class Movie ():
+	"""This Movie class creates instances of movies and their information"""
+	def __init__(self, title, movieAbout, trailer_youtube_url, poster_image_url):
+		self.title = title
+		self.movieAbout = movieAbout
+		self.trailer_youtube_url = trailer_youtube_url
+		self.poster_image_url = poster_image_url
