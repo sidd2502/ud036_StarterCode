df0940fbf6930f7071d5e0a37052235e1f845cfb
Siddharth Janghu <siddharthj.dce@gmail.com>
2018-01-26 00:34:22 +0530

    Submission after review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,27 +1,34 @@
 import fresh_tomatoes
 import media
 
-# Creating 3 objects of Movie in the module "media"
+"""
+Creating 3 objects of Movie in the module "media"
+"""
 
 ddlj = media.Movie("Dilwale Dulhania Le Jayenge",
-				   "Raj and Simran meet on Eurail and unknowingly fall for each other. Raj is shattered to learn that she is already engaged. He follows Simran to India in order to win her and her strict father's heart.",
+				   "Raj and Simran meet on Eurail and fall for each other.",
 				   "https://www.youtube.com/watch?v=c25GKl5VNeY&vl=en",
 				   "https://upload.wikimedia.org/wikipedia/en/8/80/Dilwale_Dulhania_Le_Jayenge_poster.jpg")
 
 swades = media.Movie("Swades",
-				   "Mohan, a project manager who is employed with NASA, travels to India to take his nanny along with him. Little does he know that this journey will change his life forever.",
+				   "Mohan, a project manager who is employed with NASA, travels to India.",
 				   "https://www.youtube.com/watch?v=qjc4a2Sg-B4",
 				   "https://upload.wikimedia.org/wikipedia/en/8/85/Swades_poster.jpg")
 
 cdi = media.Movie("Chak De! India",
-				   "Kabir Khan, a former hockey star, tainted as someone who betrayed his country, begins coaching the Indian women's national hockey team to prove his loyalty to the nation.",
+				   "A former star, who is said to have betrayed country, begins coaching.",
 				   "https://www.youtube.com/watch?v=6a0-dSMWm5g",
 				   "http://www.gstatic.com/tv/thumb/movieposters/168383/p168383_p_v8_ae.jpg")
 
-# Creating a list of 3 movie objects
+
+"""
+Creating a list of 3 movie objects
+"""
 
 allMovies = [ddlj, swades, cdi]
 
-# Sending the movie list as input to the fresh tomatoe module's function called open_movies_page
+"""
+Sending the movie list as input to fresh tomato
+"""
 
 fresh_tomatoes.open_movies_page(allMovies)
--- media.py
+++ media.py
@@ -1,9 +1,23 @@
-# Defining class Movie that creates instances of movie objects
+"""
+Defining class Movie that creates instances of movie objects
+"""
 
 class Movie ():
-	"""This Movie class creates instances of movies and their information"""
-	def __init__(self, title, movieAbout, trailer_youtube_url, poster_image_url):
-		self.title = title
+	"""
+	This Movie class creates instances of movies and their information
+	"""
+	
+	def __init__(self, 
+				 movieTitle, 
+				 movieAbout, 
+				 trailerYoutube, 
+				 posterURL):
+	
+		"""
+		Istance variables store info sent while creating objects
+		"""
+	
+		self.title = movieTitle
 		self.movieAbout = movieAbout
-		self.trailer_youtube_url = trailer_youtube_url
-		self.poster_image_url = poster_image_url
+		self.trailer_youtube_url = trailerYoutube
+		self.poster_image_url = posterURL
